b6f356518fadb6358cd816f95b363a7100b241b6
SuperBro1002 <pampudiagd@gmail.com>
2026-07-05 13:42:22 -0700

    Squashed commit of the following:
    commit 16b56c77e1046496c4ddde826003e695d999bd30
    Author: SuperBro1002 <pampudiagd@gmail.com>
    Date:   Sun Jul 5 13:41:55 2026 -0700
    
        Set UI variables
    
    commit 10c3dc1c7b2cc4118ee3143eb67f6b9d7e7a8ed0
    Merge: 2dd8b16 e37d8b7
    Author: SuperBro1002 <pampudiagd@gmail.com>
    Date:   Sun Jul 5 13:25:48 2026 -0700
    
        Merge branch 'Incremental-Updates-May-3' into July-Merge-Test-3
    
    commit e37d8b7c560bdfac43fb889e93e112674efc2706
    Author: SuperBro1002 <pampudiagd@gmail.com>
    Date:   Sat Jul 4 21:20:55 2026 -0700
    
        Updated UI prefab with changes to UI in main scene
    
    commit c8c57e01a208a28aff063b9854f0044e6bb31262
    Author: ThatAidanGuy <aidan6325@gmail.com>
    Date:   Thu Jun 25 19:28:51 2026 -0700
    
        Bottom half of the UI
    
        There is now a visual indicator of player lives and player ammo. While this is mostly self-contained scripts and assets, Player.cs was also altered, so this has to be merged manually.
    
    commit c840c21e6201af2a8aabfcef3000f54b37cd33b1
    Author: ThatAidanGuy <aidan6325@gmail.com>
    Date:   Thu May 21 01:39:09 2026 -0700
    
        Added UI for score and equip load meter
    
        Added score and high score UI and equip load meter. Also doubled the player's max equip load and added life/ammo assets. This probably doesn't need to be merged with the main branch until the bottom row UI is done, which will hopefully be sooner than this was since this wound up being simpler than expected.
    
    commit f320bbfc7eb55570b1e230d23d486f0353186df3
    Author: ThatAidanGuy <aidan6325@gmail.com>
    Date:   Sun May 3 17:36:56 2026 -0700
    
        Centered game world and added black bars for UI
    
        This is an incremental commit to adjust the camera, remove the options menu background, add black bars where UI elements will go, and resize some elements to fit the entire 16:9 aspect ratio. Just small stuff it's better to have committed than not.

--- Assets/Scripts/Scripts - System/GlobalVar.cs
+++ Assets/Scripts/Scripts - System/GlobalVar.cs
@@ -1,10 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public static class GlobalVar
+public class GlobalVar : MonoBehaviour
 {
     public static int score = 0;
-    public static int difficulty = 0;
-    //public static bool playMode = false;
+    public static int difficulty = 0; public static int highScore = 10000;
+
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI highScoreText;
+
+    void UpdateScores()
+    {
+        scoreText.text = score.ToString();
+        highScoreText.text = "HI " + highScore.ToString();
+
+    }
+
+    // S T A R T
+    void Start()
+    {
+        UpdateScores();
+    }
+
+    // U P D A T E
+    void Update()
+    {
+        //Test code to make sure score can update
+        //Replace with actual conditions that award points
+        //score += 1;
+
+        //Update high score in real time
+        if (score > highScore)
+        {
+            highScore = score;
+        }
+
+        UpdateScores();
+    }
 }
